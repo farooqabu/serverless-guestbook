c5ae1399e6f9db9879e748550e610a9dd76f490b
farooqabu <96665305+farooqabu@users.noreply.github.com>
2022-04-05 20:48:27 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://0956f5b7.us-south.apigw.appdomain.cloud/guestbook/entries';
+const apiUrl = 'https://0956f5b7.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
